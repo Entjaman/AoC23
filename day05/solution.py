c56993678cb28e22d07709a54d88d85c6a9f8c9c
Entjaman <jacobzeforsell@gmail.com>
2023-12-05 22:46:04 +0100

    skeleton

--- day05/solution.py
+++ day05/solution.py
@@ -15,18 +15,19 @@ types = {
 
 map_table = [[], [], [], [], [], [], []]
 
-map = {
-    "seed": [],
-    "soil": [],
-    "fertilizer": [],
-    "water": [],
-    "light": [],
-    "temperature": [],
-    "humidity": [],
-    "location": []
-}
+# map = {
+#     "seed": [],
+#     "soil": [],
+#     "fertilizer": [],
+#     "water": [],
+#     "light": [],
+#     "temperature": [],
+#     "humidity": [],
+#     "location": []
+# }
 
-#map = [[], [], [], [], [], [], [], []]
+items = ["seed", "soil", "fertilizer", "water", "light", "temperature", "humidity", "location"]
+#mapping = [[], [], [], [], [], [], [], []]
 
 seeds = [int(x) for x in input.split('\n')[0][7:].split()] # get first line and remove "seeds: " and then get numbers
 
@@ -41,7 +42,6 @@ for type in types:
         types[type].append([int(val) for val in line.split()])
 print(types)
 
-res = []
 # begin mapping source to destination
 for i, type in enumerate(types.values()):
     for row in type:
@@ -50,9 +50,38 @@ for i, type in enumerate(types.values()):
         length = row[2] # range length
         for j in range(length):
             map_table[i].append((source +  j, destination + j)) # source -> destination mapping
+    source_values = [val[0] for val in map_table[i]] # all source values
+    destination_values = [val[1] for val in map_table[i]] # all destination values
+    temp = max(source_values) if max(source_values) > max(destination_values) else max(destination_values)
+    if temp > len(map_table[i]):
+        for k in range(temp):
+            if k not in source_values:
+                map_table[i].append((k, k))
 
+def lookup(target):
+    for table in map_table:
+        for source, destination in table:
+            if target == source:
+                return destination
+
+location = []
+for seed in seeds:
+    temp = seed
+    print(temp)
+    for i in enumerate(map_table):
+        temp = lookup(temp)
+        print(temp)
+    location.append(temp)
+print(min(location))
+
+#print(map_table)
 # begin constructing table
-for list in map_table:
-    for source, destination in list:
-        if source in map[0]:
-            pass
+# for i, list in enumerate(map_table):
+#     for source, destination in list:
+#         if source in mapping[i] and i == 0:
+#             mapping[i+1].append({source: destination})
+#         else:
+#             mapping[i+1].append({source: source})
+# for map in mapping:
+#     print(map)
+#     print()
